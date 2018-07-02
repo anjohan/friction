cd9189b8eb3707d8a28427f6a4abcc6bcc251737
Anders Johansson <anjohan@uio.no>
2018-07-02 16:05:07 +0200

    fix rigid seems to work

--- find_normal_force.py
+++ find_normal_force.py
@@ -2,7 +2,7 @@ import numpy as np
 
 normal_force = np.loadtxt("normal_force.txt")
 
-average_normal_force = np.mean(normal_force)
+average_normal_force = np.mean(normal_force[len(normal_force) // 2:])
 
 print(average_normal_force)
 
