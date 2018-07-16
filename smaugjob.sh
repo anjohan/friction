cc7cf74f1039e15e9c9e4de5bf93ed4502ffa244
anjohan <anjohan@uio.no>
2018-07-16 19:07:51 +0200

    smaug exclude

--- smaugjob.sh
+++ smaugjob.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=aj    # Job name
-#SBATCH --ntasks=23
+#SBATCH --exclude=smaug-a2,smaug-c4,smaug-c6,smaug-d1,smaug-d2,smaug-d6
+#SBATCH --ntasks=128
 #SBATCH --time=12:05:00  # Time limit hrs:min:sec
 
 mpirun hostname
