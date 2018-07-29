64e43e743681dd9c80a12b4447915672286eff66
Anders Johansson <anjohan@uio.no>
2018-07-29 18:31:32 +0200

    cmake funker

--- hysteresis/job.sh
+++ hysteresis/job.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=normal
-#SBATCH --ntasks=4
-#SBATCH --gres=gpu:4
+#SBATCH --ntasks=6
+#SBATCH --gres=gpu:6
 #SBATCH --job-name=aj
 echo $CUDA_VISIBLE_DEVICES
-make GPUS=4
+#make GPUS=6
+
+mkdir -p build
+cd build
+cmake .. -DGPUS=6
+make
