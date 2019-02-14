8b1b55a5e1dfbebca2cf6c47fed06296e68beb56
anjohan <anjohan@uio.no>
2019-02-14 10:58:10 +0100

    linear regression

--- creep/plot.py
+++ creep/plot.py
@@ -3,32 +3,25 @@ import matplotlib.pyplot as plt
 from logplotter import find_data
 from scipy.signal import savgol_filter
 
-data = find_data("data/log.creep")
+data = find_data("data/log.creep", verbose=True)
 t = np.array(data["Step"]) * 2e-6
 z_top = np.array(data["c_com_top_layer[3]"])
+t = t[: min(len(t), len(z_top))]
+z_top = z_top[: min(len(t), len(z_top))]
 
 z_smooth = savgol_filter(z_top, len(z_top) // 50 + (1 - (len(z_top) // 50) % 2), 1)
 
-Nhalf = len(z_smooth) // 2
-exponential_bit = z_smooth[:Nhalf] - z_smooth[-1]
-a, b = np.polyfit(t[:Nhalf], np.log(exponential_bit), deg=1)
-approx_exponential = np.exp(b) * np.exp(a * t)
+start = len(z_smooth) // 10
+linear_bit = z_smooth[start:]
+a, b = np.polyfit(t[start:], linear_bit, deg=1)
+approx_linear = a * t + b
 
-plt.subplots(2, 1)
-plt.subplot(2, 1, 1)
 plt.plot(t, z_top, label="raw")
 plt.plot(t, z_smooth, label="smoothed")
+plt.plot(t, approx_linear, label="linear fit")
 plt.legend()
 plt.xlabel(r"t [ns]")
 plt.ylabel(r"z$_{\mathrm{CM}}$ of top layer [Å]")
 plt.title(r"z$_{\mathrm{CM}}$ of top layer")
-
-plt.subplot(2, 1, 2)
-plt.semilogy(t, z_smooth - z_smooth[-1], label="data")
-plt.semilogy(t, approx_exponential, label=f"exp({a:.2f}*t) fit")
-plt.legend()
-plt.xlabel(r"t [ns]")
-plt.ylabel(r"z$_{\mathrm{CM}}$ difference from final position")
-plt.title(r"z$_{\mathrm{CM}}$ difference from final position, semilog with fit")
 plt.tight_layout()
 plt.savefig("z_plot.png")
