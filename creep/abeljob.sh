567be1b0ef950163ad581568c7e1808e8625df9f
Anders Johansson <anjohan@uio.no>
2019-03-13 16:40:58 +0100

    failed attempt at creep with passivation

--- creep/abeljob.sh
+++ creep/abeljob.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 # Number of tasks (MPI ranks):
-#SBATCH --time='7-00:00:00'
-# #SBATCH --account=nn9272k
+#SBATCH --time='0-04:00:00'
+#SBATCH --account=nn9272k
+#SBATCH --nodes=8
+# #SBATCH --account=trocks
 # #SBATCH --nodes=20
-#SBATCH --account=trocks
-#SBATCH --ntasks=225
-# #SBATCH --nodes=5
-# #SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=anjohan@uio.no
 #SBATCH --mem-per-cpu=3600M
 source /cluster/bin/jobsetup
 #module load intelmpi.intel
-module load intel
-module load python3
+module load intel/2018.1
+module load intelmpi.intel
 
-make
+make data/restart.creep_passivated_0
