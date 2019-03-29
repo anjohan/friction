aa3c35c132be928b24d87132d56537426d98dbb3
anjohan <anjohan@uio.no>
2019-03-29 12:30:01 +0100

    brannalarm

--- creep/find_z_pressure.py
+++ creep/find_z_pressure.py
@@ -1,9 +1,15 @@
 import numpy as np
+import sys
 
-z_pressure = np.loadtxt("data/z_pressure.txt")
+suffix = ""
+
+if len(sys.argv) > 1:
+    suffix = sys.argv[1]
+
+z_pressure = np.loadtxt(f"data/z_pressure{suffix}.txt")
 
 average_z_pressure = np.mean(z_pressure)
 
 print(average_z_pressure)
 
-np.savetxt("data/average_z_pressure.txt", [average_z_pressure], fmt="%f")
+np.savetxt(f"data/average_z_pressure{suffix}.txt", [average_z_pressure], fmt="%f")
