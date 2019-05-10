7feaeea660433653befa0d04e1a369972f3237a3
Anders Johansson <anjohan@uio.no>
2019-05-10 15:05:01 +0200

    attempt at introducing temperature dependence

--- creep/find_z_pressure.py
+++ creep/find_z_pressure.py
@@ -3,13 +3,16 @@ import sys
 
 suffix = ""
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     suffix = sys.argv[1]
+    temp = int(sys.argv[2])
+else:
+    temp = int(sys.argv[1])
 
-z_pressure = np.loadtxt(f"data/z_pressure{suffix}.txt")
+z_pressure = np.loadtxt(f"data/z_pressure{suffix}_T{temp}.txt")
 
 average_z_pressure = np.mean(z_pressure)
 
 print(average_z_pressure)
 
-np.savetxt(f"data/average_z_pressure{suffix}.txt", [average_z_pressure], fmt="%f")
+np.savetxt(f"data/average_z_pressure{suffix}_T{temp}.txt", [average_z_pressure], fmt="%f")
