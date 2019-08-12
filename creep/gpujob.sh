21865e63bcb2a32847c1afa4208bc20ad59c0503
Anders Johansson <anjohan@uio.no>
2019-08-12 11:33:40 +0200

    quartz bigfacet run

--- creep/gpujob.sh
+++ creep/gpujob.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=normal
-#SBATCH --ntasks=4
-#SBATCH --gres=gpu:4
+#SBATCH --ntasks=3
+#SBATCH --gres=gpu:3
 #SBATCH --job-name=creep
 #SBATCH --cpus-per-task=2
 echo $CUDA_VISIBLE_DEVICES
-make GPUS=4
+make GPUS=3 ITERATIONS=50 data/restart.creep_T$1_50
