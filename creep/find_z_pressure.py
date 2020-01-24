2d6a102d4022907eef7866ee3d6ce7b74e13c8bd
Anders Johansson <anjohan@uio.no>
2020-01-24 14:16:13 -0500

    attempt at rewrite for varying indents

--- creep/find_z_pressure.py
+++ creep/find_z_pressure.py
@@ -3,16 +3,22 @@ import sys
 
 suffix = ""
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     suffix = sys.argv[1]
     temp = int(sys.argv[2])
+    indent = sys.argv[3]
 else:
     temp = int(sys.argv[1])
+    indent = sys.argv[2]
 
-z_pressure = np.loadtxt(f"data/z_pressure{suffix}_T{temp}.txt")
+z_pressure = np.loadtxt(f"data/z_pressure{suffix}_T{temp}_I{indent}.txt")
 
 average_z_pressure = np.mean(z_pressure)
 
 print(average_z_pressure)
 
-np.savetxt(f"data/average_z_pressure{suffix}_T{temp}.txt", [average_z_pressure], fmt="%f")
+np.savetxt(
+    f"data/average_z_pressure{suffix}_T{temp}_I{indent}.txt",
+    [average_z_pressure],
+    fmt="%f",
+)
